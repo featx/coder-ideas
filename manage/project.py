5ab8713e758d06f72a6dc3e76986e198116fa557
Excepts <excepts@featx.org>
2020-06-18 18:09:53 +0800

    file generate ok but not test

--- manage/project.py
+++ manage/project.py
@@ -73,16 +73,42 @@ class ProjectManager:
         pass
 
     def generate(self, param):
+        project, domains, templates = self.__find_project_domains_templates(param)
+        if project is None:
+            return
+        project_dir = os.path.join(self.__git_workspace, project.code)
+        for template in templates:
+            template_path = os.path.join(project_dir, template.path)
+            template_dir, template_file = os.path.split(template_path)
+            for domain in domains:
+                data = _render_data(template.data, domain)
+                domain_file = _replace_data(template_file, data)
+                _copy_and_replace(template_path, os.path.join(template_dir, domain_file), data)
+
+    def __find_project_domains_templates(self, param):
         domain = self.__domain_service.find_by_code(param.domain_code)
         template = self.__template_service.find_by_code(param.template_code)
+        domains = []
+        templates = []
         if domain is None and template is None:
             project = self.__project_service.find_by_code(param.project_code)
+            if project is None:
+                return None, None, None
+            domains = self.__domain_service.find_by_project_code(param.project_code)
+            templates = self.__template_service.find_by_project_code(param.project_code)
         elif domain is None:
             project = self.__project_service.find_by_code(template.project_code)
+            domains = self.__domain_service.find_by_project_code(param.project_code)
+            templates.append(template)
+        elif template is None:
+            project = self.__project_service.find_by_code(domain.project_code)
+            domains.append(domain)
+            templates = self.__template_service.find_by_project_code(param.project_code)
         else:
             project = self.__project_service.find_by_code(domain.project_code)
-        project_dir = os.path.join(self.__git_workspace, project.code)    
-        pass
+            domains.append(domain)
+            templates.append(template)
+        return project, domains, templates
 
 
 def _to_project(creating_project):
@@ -99,3 +125,26 @@ def _to_project(creating_project):
         branch=creating_project.branch,
         comment=creating_project.comment
     )
+
+def _render_data(template: dict, domain):
+    data = dict()
+    for key, value in template.items():
+        k = key.replace("${domain.name}", domain.name)
+        v = value.replace("${domain.name}", domain.name)
+        data[k] = v
+    return data
+
+def _replace_data(origin: str, data: dict):
+    result = origin
+    for k, v in data.items():
+        result = result.replace(k, v)
+    return result
+
+def _copy_and_replace(src: str, dst: str, map: dict):
+    with open(src, "r", encoding="utf-8") as read:
+        with open(dst, "w", encoding="utf-8") as write:
+            for line in read:
+                write.write(_replace_data(line, map))
+                write.flush()
+            write.close()
+        read.close()
